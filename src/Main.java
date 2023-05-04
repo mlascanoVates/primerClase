1288502b0d87a9cce2ce04e582e7861a038319e1
mlascanoVates <132603421+mlascanoVates@users.noreply.github.com>
2023-05-04 17:48:43 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -67,6 +67,29 @@ public class Main {
         System.out.println("El resultado de su cociente es: " + cociente);
         System.out.println();
 
+        
+        //CALCULADORA OP2
+        System.out.println("Lee con Scanner dos números desde archivo y se muestra el resultado de la suma, diferencia, producto y división: ");
+        File archivo2 = new File("Leer.txt");
+        Scanner leerArchivo2 = new Scanner(archivo2);
+
+        int linea3, linea4;
+
+        linea3 = Integer.parseInt(leerArchivo2.nextLine());
+        linea4 = Integer.parseInt(leerArchivo2.nextLine());
+        System.out.println("Primer número es: " + linea3);
+        System.out.println("Segundo número es: " + linea4);
+
+        suma= linea3 + linea4;
+        diferencia=linea3 - linea4 ;
+        producto= linea3 * linea4;
+        cociente= (double)linea3 / linea4;
+
+        System.out.println("El resultado de su suma es: " + suma);
+        System.out.println("El resultado de su diferencia es: " + diferencia);
+        System.out.println("El resultado de su producto es: " + producto);
+        System.out.println("El resultado de su cociente es: " + cociente);
+        System.out.println();
 
         //MAYOR DE 2
         Scanner escanearNumero= new Scanner(System.in);
@@ -82,8 +105,25 @@ public class Main {
         //uso clase max de la biblioteca Math
         mayor=  max(numeroEscaneado1,numeroEscaneado2);
 
-        System.out.print("El número mayor es: " + mayor);
+        System.out.println("El número mayor es: " + mayor);
 
+        //OTRA FORMA DE HACER
+        //Solo recibe números positivos
+        System.out.println();
+        mayor = ( abs(numeroEscaneado1 + numeroEscaneado2) + abs( numeroEscaneado1 - numeroEscaneado2 ) ) / 2;
+
+        System.out.println("Otra manera de imprimir mayor 1: " + mayor);
+
+        System.out.println("Otra manera de imprimir mayor 2: " + mayor(numeroEscaneado1,numeroEscaneado2));
+
+    }
+    
+    public static int mayor(int n, int m){
+        int a,b,c;
+        a= abs(n+m);
+        b =abs(n-m);
+        c= (a+b)/2;
+        return c;
     }
 
 }
